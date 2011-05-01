ad6d04efe29916eef964ade0b778ffd2d18c87a3
Isaac J <icarusmod@gmail.com>
2011-04-30 20:19:11 -0500

    missed something again

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -21,7 +21,7 @@ ifneq ($(TARGET_SIMULATOR),true)
 # hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := sensors.u8220
+LOCAL_MODULE := sensors.M860
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
